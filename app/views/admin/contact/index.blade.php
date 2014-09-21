f5eddb99af4edd03bcfc01d26eebb47e67177218
Arif <arif04cuet@gmail.com>
2014-09-21 15:13:39 +0600

    as

--- app/views/admin/contact/index.blade.php
+++ app/views/admin/contact/index.blade.php
@@ -12,15 +12,56 @@
     #contact-lookup .control-label {
         text-align: left
     }
-    .contact-types .btn:hover{ background-position: 0px !important;}
+    .contact-types .btn:hover {
+        background-position: 0px !important;
+    }
+    .table-responsive
+    {
+        overflow-x: auto;
+    }
+    @-moz-document url-prefix() {
+        fieldset { display: table-cell; }
+    }
 </style>
+
+<nav class="navbar navbar-inverse hidden-md hidden-lg" role="navigation">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                    data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Account Types</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav contact-types">
+                <li><a href="{{{ URL::to('admin/contacts/?type=1') }}}" class=""><span
+                            class="glyphicon glyphicon-search"></span> My RSC Accounts</a></li>
+                <li><a href="{{{ URL::to('admin/contacts/?type=2') }}}" class=""><span
+                            class="glyphicon glyphicon-search"></span> My DSC Accounts</a></li>
+                <li><a href="{{{ URL::to('admin/contacts/?type=3') }}}" class=""><span
+                            class="glyphicon glyphicon-search"></span> My Accounts</a></li>
+                <li><a href="{{{ URL::to('admin/contacts/?type=4') }}}" class=""><span
+                            class="glyphicon glyphicon-search"></span> My Leads</a></li>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container-fluid -->
+</nav>
 
 <div class="page-header">
     <h3>
         {{{ $title }}}
 
 
-        <div class="pull-right">
+        <div class="pull-right hidden-xs hidden-sm">
             <ul class="nav nav-pills contact-types">
                 <li><a href="{{{ URL::to('admin/contacts/?type=1') }}}" class="btn btn-small btn-info "><span
                             class="glyphicon glyphicon-search"></span> My RSC Accounts</a></li>
@@ -135,9 +176,9 @@
                         <label for="state" class="col-sm-4 control-label">State</label>
 
                         <div class="col-sm-8">
-                           <!-- <select name="state" id="state" class="form-control">
-                                <option>Select</option>
-                            </select>-->
+                            <!-- <select name="state" id="state" class="form-control">
+                                 <option>Select</option>
+                             </select>-->
                             {{ Form::select('state', $states , Input::old('state')) }}
                         </div>
                     </div>
@@ -211,23 +252,25 @@
 
 </div>
 <hr/>
-<table id="contacts" class="table table-striped table-hover table-condensed">
-    <thead>
-    <tr>
-        <th class="col-md-2">Company</th>
-        <th class="col-md-2">Contact Name</th>
-        <th class="col-md-2">Account #</th>
-        <th class="col-md-2">Address</th>
-        <th class="col-md-2">City</th>
-        <th class="col-md-2">State</th>
-        <th class="col-md-2">Country</th>
-        <th class="col-md-2">Type</th>
-        <th class="col-md-2">Actions</th>
-    </tr>
-    </thead>
-    <tbody>
-    </tbody>
-</table>
+<div class="table-responsive">
+    <table id="contacts" class="table table-striped table-hover table-condensed">
+        <thead>
+        <tr>
+            <th class="col-md-2">Company</th>
+            <th class="col-md-2">Contact Name</th>
+            <th class="col-md-2">Account #</th>
+            <th class="col-md-2">Address</th>
+            <th class="col-md-2">City</th>
+            <th class="col-md-2">State</th>
+            <th class="col-md-2">Country</th>
+            <th class="col-md-2">Type</th>
+            <th class="col-md-2">Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+        </tbody>
+    </table>
+</div>
 @stop
 
 {{-- Scripts --}}
